a249ee78055021b514ef84c6f56f394d1dfffb36
rahul <rahul.valasai@gmail.com>
2022-06-06 12:50:56 +0530

    quadratic.

---
+++ quadratic.js
@@ -0,0 +1,17 @@
+//quadratic equation
+
+let a = 1,b = 3,c = -10;
+
+y = (b**2)-(4*a*c);
+
+console.log(a,b,c);
+
+let e = Math.sqrt(y);
+
+console.log("sqrt of y is:",e);
+
+x1 = ((-b+e)/(2*a));
+
+x2 = ((-b-e)/(2*a));
+
+console.log(x1,x2);
